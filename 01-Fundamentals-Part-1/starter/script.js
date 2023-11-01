6d8e9eb757a65c315f927a02fec7b85381db24f0
HolyJack <holyjackdev@gmail.com>
2023-11-01 14:56:31 +0400

    part1 progress

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,46 @@
-let js = "nice";
-if (js === "nice") alert(`JS is ${js}`);
+const country = "Russia";
+const continent = "Eurasia";
+const population = 143400000;
+
+console.log(
+  `Country: ${country}. Continent: ${continent}. Population: ${population}`,
+);
+
+const isIsland = false;
+const language = "russian";
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language,
+);
+
+const finland_population = 6000000;
+const average_population = 33000000;
+const half_population = population / 2;
+const population_with_extra_one = population + 1;
+const population_more_then_finland = population > finland_population;
+const population_more_then_avarage = population > average_population;
+
+const description = `${country} is in ${continent}, and it's ${
+  population / 1000000
+} million people speak ${language}.`;
+
+if (population_more_then_avarage) {
+  console.log(`${country}'s population is above average.'`);
+} else {
+  console.log(
+    `${country}'s poplation is ${
+      (average_population - population) / 1000000
+    } below average'`,
+  );
+}
+
+console.log(
+  "9" - "5",
+  "19" - "13" + "17",
+  "19" - "13" + 17,
+  "123" < 57,
+  5 + 6 + "4" + 9 - 4 - 2,
+);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,17 +1,1 @@
-let firstName = "lalala";
-
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-
-let jonas_matilda = "JM";
-let $function = 27;
-
-let person = "jonas";
-let PI = 3.1415;
-
-let myFirstJob = "Coder";
-let myCurrentJob = "teacher";
-
-console.log(myFirstJob);
-console.log(myCurrentJob);
+const isIsland = "false";
